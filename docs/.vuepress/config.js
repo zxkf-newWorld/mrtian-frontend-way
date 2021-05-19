ede4af4b84bee7096ed6b493847622bab22004e9
hepeng <18309225861@163.com>
2021-05-19 10:25:14 +0800

    添加config.js配置文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  base: '/mrtian-frontend-way/'
+  base: '/mrtian-frontend-way/', // 默认自定部署到gh-pages
+  title: '前端之风',
+  description: '天净若风的前端之路',
+  head: [
+    ['link', { rel: 'icon', href: '/logo.png' }]
+  ]
 }
