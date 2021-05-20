8ff52ab1fd59fbbc4abc9489c3bacf1abb3bbbf6
zxkf-newWorld <18309225861@163.com>
2021-05-21 00:10:44 +0800

    添加base环境变量配置

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   base: process.env.NODE_ENV === 'development' ? '/' : '/mrtian-frontend-way/',
-  // base: '/mrtian-frontend-way/',
   title: '天净若风--前端之路',
   description: '天净若风的前端之路',
 }
