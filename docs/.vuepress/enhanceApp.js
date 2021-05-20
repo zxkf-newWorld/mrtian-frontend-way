a02357884c1a2b6c642c4d7d1011e343787d6be4
zxkf-newWorld <18309225861@163.com>
2021-05-20 22:50:20 +0800

    添加vuepress的全局配置

---
+++ docs/.vuepress/enhanceApp.js
@@ -0,0 +1,8 @@
+/*
+ * @Author: hp
+ * @Date: 2021-05-20 22:29:40
+ * @LastEditTime: 2021-05-20 22:29:41
+ * @LastEditors: your name
+ * @Description:
+ * @FilePath: /Front-end-of-mrtian/docs/.vuepress/enhanceApp.js
+ */
---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: hp
+ * @Date: 2021-05-20 22:28:17
+ * @LastEditTime: 2021-05-20 22:40:02
+ * @LastEditors: your name
+ * @Description: 天净若风的
+ * @FilePath: /Front-end-of-mrtian/docs/.vuepress/theme/Layout.vue
+-->
+<template>
+	<div class='public-layout'>
+		hello vuepress
+		<say-hello></say-hello>
+	</div>
+</template>
+
+<script>
+import SayHello from '../components/SayHello.vue'
+export default {
+	components: { SayHello },
+	name: 'publiclayout',
+	data() {
+		return {}
+	},
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
